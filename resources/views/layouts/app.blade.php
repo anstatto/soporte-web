abc2006a15d24170b1a88a3d7bad64434c7d99b8
anstatto <angeltatistorres@gmail.com>
2024-10-21 13:25:50 -0400

    subimos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>RM Consuegra SRL Soporte</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="{{ asset('images/LogoMono.png') }}" type="image/x-icon">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100 min-h-screen font-sans antialiased">
